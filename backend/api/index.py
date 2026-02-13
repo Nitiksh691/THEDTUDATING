aafb671160efec512084b68b25df0b42629a102f
nitiksh <iamnitiksh@yahoo.com>
2026-02-13 21:17:28 +0530

    Fix Vercel build config

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the parent directory to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from main import app
 
 # This file is the entry point for Vercel Serverless Functions.
